dc7dd81055fd6af4a23336d97b0a4d816d8d7cd2
Clavichord-we3 <93472796+Clavichord-we3@users.noreply.github.com>
2021-11-14 20:56:43 +0530

    Add files via upload

--- html_link.py
+++ html_link.py
@@ -1,14 +1,10 @@
-from flask import Flask,request,render_template
+from flask import Flask, request, render_template
 from flaskext.mysql import MySQL
 mysql = MySQL()
 
 from music_samples import *
 
 app = Flask(__name__)
-app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
-app.config['MYSQL_DATABASE_DB'] = 'weprojectdb'
-app.config['MYSQL_DATABASE_host'] = '127.0.0.1:3306'
 mysql.init_app(app)
 
 @app.route('/',methods = ['GET','POST'])
@@ -16,17 +12,12 @@ def get_data():
   if request.method == 'POST':
     melody = request.form['Melody']
     bass = request.form['Bass']
-    play_music(list(melody), list(bass), 0.5)
-    print(list(melody))
-    print(list(bass))
-    connection = mysql.get_db()
-    cursor = connection.cursor()
-    query = "INSERT INTO store_inputs(Melody, Bass) VALUES(%s,%s)"
-    cursor.execute(query,(melody, bass))
-    connection.commit()
+    form_data = dict(request.form)
+    melody = form_data['Melody'].split(",")
+    bass = form_data['Bass']
+    play_music(list(map(int, melody)), list(eval(bass)), 0.5)
 
   return render_template("index.html")
 
 if __name__ == '__main__':
     app.run(debug = True)
-#add-content-to-html-link
